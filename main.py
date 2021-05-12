ab38daae4f7b80405337791d57eb0fe5657b33ad
David <davidkoh347@gmail.com>
2021-05-12 13:48:45 +0200

    Sound game

--- main.py
+++ main.py
@@ -49,3 +49,24 @@ for i in range(1,3):
             grovepi.digitalWrite(redLed,0)
             print("Your reaction time is "+str(time)+"ms")
             #enregistrer résultat+ écrire dans csv
+
+#SOUND REACTION
+input("Vous allez commencez le test de réaction au son, appuyer sur ENTER pour commencer")
+for i in range(1,3):
+    time=0
+    condition=True
+    print("Le son va s'allumer, préparez vous à appuyer sur le bouton!")
+    sleep(1.5)
+    sleep(uniform(2.5, 6))
+    grovepi.digitalWrite(redLed,1)
+
+    while condition:
+        button_status=grovepi.digitalRead(button)
+        time+=1
+        sleep(0.001)
+        if button_status==True:
+            condition=False
+            grovepi.digitalWrite(buzzer,0)
+            print("Your reaction time is "+str(time)+"ms")
+            #enregistrer résultat+ écrire dans csv
+
